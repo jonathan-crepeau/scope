331efaca49648501779d271dac874c8f50a3d6ac
jonathan-crepeau <jon.crepeau@gmail.com>
2024-02-23 11:37:10 -0800

    Scope exercise #1 completed

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+// ANCHOR - Scope Exercises (SEI)
+
+// SECTION Exercise 1: Define a variable and try to access it from a scope where it CANNOT be accessed:
+
+// {
+//   let myName = 'Jonathan';
+//   const age = 35;
+//   var myLocation = 'California';
+// }
+// console.log(myLocation);
+// console.log(myName);
+// console.log(age);
+
+
 
