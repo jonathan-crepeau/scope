f47b47c662915933e8bbd03707f2fc3a343225a4
jonathan-crepeau <jon.crepeau@gmail.com>
2024-02-23 12:09:09 -0800

    Scope exercise #2 completed

--- app.js
+++ app.js
@@ -14,4 +14,14 @@
 // console.log(age);
 
 
+// SECTION Exercise 2: Write a function that returns a variable declared and defined inside of a function -- will it return that value when the function is called?
+// NOTE - It will return the value, but you cannot access the variable itself from within the function (see console logs below):
+
+// const getPasta = () => {
+//   const pasta = 'Ravioli';
+//   return pasta;
+// }
+// console.log(getPasta());
+// console.log(pasta);
+
 
